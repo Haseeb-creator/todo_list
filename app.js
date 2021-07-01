c3fa543378920eddfe353ded68b2c8e2fe14f09a
Haseeb-creator <80565984+Haseeb-creator@users.noreply.github.com>
2021-07-01 14:52:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItem = [];
+
+app.set("view engine", "ejs");
+
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  const day = date.getDate();
+
+  res.render("list", { listTitle: day, newListItems: items });
+});
+
+app.post("/", function (req, res) {
+ const item = req.body.newItem;
+
+  if (req.body.list === "work") {
+    workItem.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "work list", newListItems: workItem });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+app.post("/work", function (req, res) {
+  let item = req.body.newItem;
+  workItem.push(item);
+  res.redirect("/work");
+});
+
+app.listen(3000, function () {
+  console.log("server started on port 3000");
+});
